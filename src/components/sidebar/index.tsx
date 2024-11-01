c2d15fdd914b8a322a2e5f3d812e7b40ae8453df
rickynguyen <ricky.nguyen@handshakes.com.vn>
2024-11-01 16:49:57 +0700

    form

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,4 +1,4 @@
-import { BarChart, MenuIcon, User, User2, User2Icon, UserSquare2 } from "lucide-react"
+import { BarChart, Link, Menu, MenuIcon, User, User2, User2Icon, UserSquare2 } from "lucide-react"
 import { Route, Routes } from "react-router-dom"
 import Homepage from "../homepage"
 import { motion } from "framer-motion"
@@ -15,26 +15,45 @@ export const Sidebar = () => {
   const [isSideBarOpen, setIsSideBarOpen] = useState(true)
 
   return (
-    <div>
-       {/* <Routes>
-            {SIDEBAR_ITEMS.length > 0 && SIDEBAR_ITEMS.map((item) => {
-              <Route path={item.path} element={/>
-            })}
-
-        </Routes> */}
         <motion.div
           className={`relative z-10 transition-all duration-300 ease-in-out flex-shrink-0 ${isSideBarOpen ? 'w-64' : 'w-20'}`}
           animate={{ width: isSideBarOpen ?  256 : 80}}
         >
-
+          
         <div className="h-full bg-gray-800 bg-opacity-50 backdrop-blur-md p-4 flex flex-col border-r border-gray-700">
-          <motion.button whileHover={{scale: 1.1}} whileTap={{scale: 0.9}} onClick={() => setIsSideBarOpen(!isSideBarOpen)} className="p-2 rounded-full hover:bg-gray-700 transition-colors max-w-fit">
+          <motion.button 
+          whileHover={{scale: 1.1}} 
+          whileTap={{scale: 0.9}} 
+          onClick={() => setIsSideBarOpen(!isSideBarOpen)} 
+          className="p-2 rounded-full hover:bg-gray-700 transition-colors max-w-fit">
 
-          <MenuIcon></MenuIcon>
+          <Menu size={24}/>
           </motion.button>
-        </div>
 
+          {/* <Routes> */}
+        <nav className="mt-8 flex-grow">
+            {SIDEBAR_ITEMS.map((item, index )=> (
+            <Link key={item.path} to={item.path}>
+              <motion.div className="flex p-4 text-sm font-medium rounded-lg hover:bg-gray-400">
+
+                    <item.icon size={20} style={{color: item.color, minWidth: "20px"}}/> 
+
+              </motion.div>
+            </Link>
+
+            ))}
+        </nav>
+{/* <AnimatePresence>
+                      {isSideBarOpen && (
+                        <motion.span initial={{ opacity: 0, width: 0}} animate={{ opacity: 0, width: 0}} exit={{ opacity: 0, width: 0}}>
+
+                        </motion.span>
+                      )}
+                    </AnimatePresence> */}
+        {/* <Route element='' path={item.path}> {item.name} </Route> */}
+          {/* </Routes> */}
+        </div>
+        
         </motion.div>
-    </div>  
   )
 }
